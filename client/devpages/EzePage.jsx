3eedae1286457a71f3301bd0d9f708f10ed9ecff
RichardIrala <richardirala47@hotmail.com>
2022-08-10 12:44:52 -0300

    Se creo, configuro e instancio el Router de la app + devpages and pages

---
+++ client/devpages/EzePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function EzePage() {
+  return (
+    <View>
+      <Text>Esta página esta reservada para vos EZE.</Text>
+      <Text>Desarrolla tranqui</Text>
+    </View>
+  );
+}
---
+++ client/devpages/OrianaPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function OrianaPage() {
+  return (
+    <View>
+      <Text>Esta página esta reservada para vos ORIANA.</Text>
+      <Text>Desarrolla tranqui</Text>
+    </View>
+  );
+}
