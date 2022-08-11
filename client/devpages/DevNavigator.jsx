d74c873d1d5927b8c453870242763e087ed564bf
RichardIrala <richardirala47@hotmail.com>
2022-08-10 22:37:36 -0300

    Corrección al scrollView

--- client/devpages/DevNavigator.jsx
+++ client/devpages/DevNavigator.jsx
@@ -1,28 +1,51 @@
 import React from "react";
 import { Text, View, ScrollView, StyleSheet } from "react-native";
 import { Link } from "react-router-native";
+import { Dimensions } from "react-native";
 
+const ScreenWidth = Dimensions.get("window").width;
 export default function DevNavigator() {
   return (
-    <View>
+    <View style={{}}>
       <ScrollView horizontal style={styles.scrollView}>
         <View style={styles.contentContainer}>
           <Link style={styles.link} to="/" underlayColor="rgba(0,0,0,0)">
             <Text style={styles.redirectTo}>Volver al inicio</Text>
           </Link>
-          <Link style={styles.link} to="/ClaudiaPage" underlayColor="rgba(0,0,0,0)">
+
+          <Link
+            style={styles.link}
+            to="/ClaudiaPage"
+            underlayColor="rgba(0,0,0,0)"
+          >
             <Text style={styles.redirectTo}>Claudia Page</Text>
           </Link>
-          <Link style={styles.link} to="/CrisPage" underlayColor="rgba(0,0,0,0)">
+
+          <Link
+            style={styles.link}
+            to="/CrisPage"
+            underlayColor="rgba(0,0,0,0)"
+          >
             <Text style={styles.redirectTo}>Cris Page</Text>
           </Link>
+
           <Link style={styles.link} to="/EzePage" underlayColor="rgba(0,0,0,0)">
             <Text style={styles.redirectTo}>Eze Page</Text>
           </Link>
-          <Link style={styles.link} to="/OrianaPage" underlayColor="rgba(0,0,0,0)">
+
+          <Link
+            style={styles.link}
+            to="/OrianaPage"
+            underlayColor="rgba(0,0,0,0)"
+          >
             <Text style={styles.redirectTo}>Oriana Page</Text>
           </Link>
-          <Link style={styles.link} to="/RichardPage" underlayColor="rgba(0,0,0,0)">
+
+          <Link
+            style={styles.link}
+            to="/RichardPage"
+            underlayColor="rgba(0,0,0,0)"
+          >
             <Text style={styles.redirectTo}>Richard Page</Text>
           </Link>
         </View>
@@ -32,20 +55,21 @@ export default function DevNavigator() {
 }
 
 const styles = StyleSheet.create({
+  scrollView: { backgroundColor: "red", paddingBottom: 15, marginTop: 20 },
   link: {
     marginEnd: 20,
+    width: 200,
   },
   contentContainer: {
     flexDirection: "row",
-    height: 100,
-    alignItems: "center",
     paddingLeft: 20,
+    width: 300 * 6,
+    alignItems: "center",
   },
 
   redirectTo: {
     backgroundColor: "#C20FE1",
     padding: 10,
-    width: 100,
     textAlign: "center",
     borderRadius: 4,
     marginBottom: 10,
