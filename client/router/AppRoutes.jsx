2dae3ce187c87344c929b6f5fd59efa9ba317184
orianadecaro <orianadecaro@gmail.com>
2022-08-11 00:30:11 -0300

    change approute-cris

--- client/router/AppRoutes.jsx
+++ client/router/AppRoutes.jsx
@@ -8,13 +8,14 @@ import SecundaryRoutes from "./secundaryRoutes";
 import Login from "../pages/Login.jsx";
 import Signup from "../pages/Signup.jsx";
 import ForgotPassword from "../pages/ForgotPassword.jsx";
-
+import CrisPage from "../devpages/CrisPage.jsx";
 
 export default function AppRoutes() {
   return (
     <>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
       <Routes>
+       <Route path="/CrisPage" element={<CrisPage />} />
         <Route path="/sisisi" element={<RichardPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
