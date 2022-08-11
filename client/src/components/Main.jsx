ae3b7e0bef9ce5d828fa3409ec74a47d6ec64801
“clambeta” <claudiambetam@gmail.com>
2022-08-10 20:02:20 -0500

    adición archivos nuevos

---
+++ client/src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Text, View} from 'react-native'
+import ListaProductos from "./ListaProductos";
+
+const Main = () => {
+    return (
+        <View>
+            <ListaProductos />
+        </View>
+    )
+}
+
+export default Main
