156012f70f532dd537f014b1c9b133899aa5124e
RichardIrala <richardirala47@hotmail.com>
2022-08-10 23:35:49 -0300

    Correción de separación entre statusBar y Cuerpo de la app

--- client/router/secundaryRoutes.jsx
+++ client/router/secundaryRoutes.jsx
@@ -16,49 +16,46 @@ import CrisPage from "../devpages/CrisPage.jsx";
 import OrianaPage from "../devpages/OrianaPage.jsx";
 import RichardPage from "../devpages/RichardPage.jsx";
 import Nav from "../components/Nav";
-import Profile from '../pages/Profile.jsx'
+import Profile from "../pages/Profile.jsx";
 import Orders from "../pages/Orders";
 import { StatusBar } from "expo-status-bar";
+import { StatusBar as barraDeEstado } from "react-native";
 
 const ScreenHeight = Dimensions.get("window").height;
+
 export default function SecundaryRoutes() {
   return (
     <View style={styles.container}>
       <Search />
-        <StatusBar style="auto" />
-          <Routes>
-            <Route path="/" element={<Homepage />}></Route>
-            <Route path="/ClaudiaPage" element={<ClaudiaPage />} />
-            <Route path="/CrisPage" element={<CrisPage />} />
-            <Route path="/EzePage" element={<Profile />} />
-            <Route path="/EzePage/orders" element={<Orders />} />
-            <Route path="/OrianaPage" element={<OrianaPage />} />
-            <Route path="/RichardPage" element={<RichardPage />} />
-            <Route path="*" element={<Text>Ruta Global</Text>} />
-          </Routes>
+      <StatusBar style="auto" />
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/ClaudiaPage" element={<ClaudiaPage />} />
+        <Route path="/CrisPage" element={<CrisPage />} />
+        <Route path="/EzePage" element={<Profile />} />
+        <Route path="/EzePage/orders" element={<Orders />} />
+        <Route path="/OrianaPage" element={<OrianaPage />} />
+        <Route path="/RichardPage" element={<RichardPage />} />
+        <Route path="*" element={<Text>Ruta Global</Text>} />
+      </Routes>
       <Nav />
     </View>
   );
-  }
-  const styles = StyleSheet.create({
-    container: {
-      paddingVertical: 25,
-      marginTop : StatusBar.currentHeight,
-      height: ScreenHeight,
-      flex: 1,
-      backgroundColor:'#f3f3f3'
-    },
-    secundaryContainer: {
-      alignItems: "center",
-    },
-    texto: {
-      textAlign: "center",
-      color: "#fff",
-      fontSize: 16,
-      width: 100,
-    },
-    width200: {
-      width: 200,
-    },
-  });
-
+}
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: barraDeEstado.currentHeight,
+    height: ScreenHeight,
+    flex: 1,
+    backgroundColor: "#f3f3f3",
+  },
+  secundaryContainer: {
+    alignItems: "center",
+  },
+  texto: {
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 16,
+    width: 100,
+  },
+});
