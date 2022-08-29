acc06e409f59b054099fa2b45601996a4484b409
cristian <cristiangomez.dev@gmail.com>
2022-08-29 12:59:47 -0300

    fix

--- client/data/productosDetail.js
+++ client/data/productosDetail.js
@@ -1,54 +1,54 @@
 
-// const productos = [{
-//     id: 1,
-//     name : 'Alimento seco para perros, carne de res y arroz integral, bolsa de 1001010010 libras',
-//     price : 6.99,
-//     size: '2.3 kg',
-//     brand: 'Wag',
-//     image : '/assets/imgs/Alimento1.jpg'
-//     },
-//     {
-//     id: 2,
-//     name : 'Alimento seco Life Protection de fórmula natural para perros adultos, con pollo y arroz integral, 15 libras',
-//     price : 23.99,
-//     size: '6.8 kg',
-//     brand: 'Buffalo',
-//     image : [require('../assets/imgs/Alimento2.jpg')]
-//     },
-//     {
-//     id: 3,
-//     name : 'Comida para perros de raza peque',
-//     price : 13.99,
-//     size: '5.44 kg',
-//     brand: 'Cesar',
-//     image : [require('../assets/imgs/Alimento3.jpg')]
-//     },
-//     {
-//     id: 4,
-//     name : 'Alimento seco natural para perros, fórmula SmartBlend de cordero y arroz, 8 libras Bolsa',
-//     price : 11.99,
-//     size: '3.6 kg',
-//     brand: 'Purina ONE',
-//     image : [require('../assets/imgs/Alimento4.jpg')]
-//     },
-//     {
-//     id: 5,
-//     name : 'Alimento seco para perros de 45 libras, sabor salado original de carne y pollo, tamaño grande',
-//     price : 50.99,
-//     size: '20.4 kg',
-//     brand: 'Kibbles N Bits',
-//     image : [require('../assets/imgs/Alimento5.jpg')]
-//     },
-//     {
-//     id: 6,
-//     name : 'Tender Bites Complete Nutrition Alimento seco para perros adultos, sabor a pollo y filete, bolsa de 14 libras',
-//     price : 21.99,
-//     size: '6.4 kg',
-//     brand: 'PEDIGREE',
-//     image : [require('../assets/imgs/Alimento6.jpg')]
-//     }
-// ];
+ const productos = [{
+     id: 1,
+     name : 'Alimento seco para perros, carne de res y arroz integral, bolsa de 1001010010 libras',
+     price : 6.99,
+     size: '2.3 kg',
+     brand: 'Wag',
+     image : '/assets/imgs/Alimento1.jpg'
+     },
+     {
+     id: 2,
+     name : 'Alimento seco Life Protection de fórmula natural para perros adultos, con pollo y arroz integral, 15 libras',
+     price : 23.99,
+     size: '6.8 kg',
+     brand: 'Buffalo',
+     image : [require('../assets/imgs/Alimento2.jpg')]
+     },
+     {
+     id: 3,
+     name : 'Comida para perros de raza peque',
+     price : 13.99,
+     size: '5.44 kg',
+     brand: 'Cesar',
+     image : [require('../assets/imgs/Alimento3.jpg')]
+     },
+     {
+     id: 4,
+     name : 'Alimento seco natural para perros, fórmula SmartBlend de cordero y arroz, 8 libras Bolsa',
+     price : 11.99,
+     size: '3.6 kg',
+     brand: 'Purina ONE',
+     image : [require('../assets/imgs/Alimento4.jpg')]
+     },
+     {
+     id: 5,
+     name : 'Alimento seco para perros de 45 libras, sabor salado original de carne y pollo, tamaño grande',
+     price : 50.99,
+     size: '20.4 kg',
+     brand: 'Kibbles N Bits',
+     image : [require('../assets/imgs/Alimento5.jpg')]
+     },
+     {
+     id: 6,
+     name : 'Tender Bites Complete Nutrition Alimento seco para perros adultos, sabor a pollo y filete, bolsa de 14 libras',
+     price : 21.99,
+     size: '6.4 kg',
+     brand: 'PEDIGREE',
+     image : [require('../assets/imgs/Alimento6.jpg')]
+     }
+ ];
 
 
-// export default productos
+ export default productos
 
