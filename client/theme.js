381f7b510a992a5bc97a09fe6372dea70f3dc4e6
RichardIrala <richardirala47@hotmail.com>
2022-08-29 17:17:35 -0300

    Se creo un theme básico para ponerlo a prueba

---
+++ client/theme.js
@@ -0,0 +1,41 @@
+const theme = {
+  colors: {
+    //Celeste
+    principal: "#56CBF9",
+    //Blanco
+    secundary: "#FDEFE9",
+    //Negro
+    black: "#000000",
+    //Gris - Podríamos cambiarlo.
+    gray: "#808080",
+  },
+
+  fontSizes: {
+    smallBody: 14,
+    body: 16,
+    subtitle: 20,
+    title: 26,
+  },
+
+  fontWeights: {
+    classic: 400,
+    semiBold: 600,
+    bold: 700,
+  },
+
+  paddings: {
+    small: 10,
+    smallV2: 20,
+    medium: 40,
+    long: 60,
+  },
+
+  margins: {
+    small: 10,
+    smallV2: 20,
+    medium: 40,
+    long: 60,
+  },
+};
+
+export default theme;
